16b87230ed94709325d48a1735f1405c5447815d
Gabriel <deng93003@gmail.com>
2026-08-15 23:35:02 +0100

    Implementation of exercise 2

---
+++ Sprint3/src/main/java/exercise2.1/GradeBook.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class GradeBook {
+    private String[] firstNames;
+    private String[] lastNames;
+    private int[] grades;
+
+    public GradeBook(String[] firstNames, String[] lastNames, int[] grades) {
+        this.firstNames = firstNames;
+        this.lastNames = lastNames;
+        this.grades = grades;
+    }
+
+    public List<String> getFirstNames() {
+        return Arrays.asList(firstNames);
+    }
+
+    public Set<String> getDistinctFirstNames() {
+        return new HashSet<>(Arrays.asList(firstNames));
+    }
+
+    public Set<String> getDistinctFirstNamesInAlphabeticalOrder() {
+        return new TreeSet<>(Arrays.asList(firstNames));
+    }
+
+    public Map<String, Integer> getLastNameToGrade() {
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < lastNames.length; i++) {
+            map.put(lastNames[i], grades[i]);
+        }
+        return map;
+    }
+}
---
+++ Sprint3/src/main/java/exercise2.1/Student.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Student {
+    private String firstName;
+    private String lastName;
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName= lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+
+        return Objects.equals(firstName, student.firstName) &&
+                Objects.equals(lastName, student.lastName);
+    }
+
+    @Override
+    public boolean hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+}
